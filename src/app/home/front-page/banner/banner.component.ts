e66b9dee03cbcea74ad9be63badefe9697d24a37
EricKhoaMonger <n.d.khoa511@gmail.com>
2018-09-24 12:56:04 +0700

    fix frontpage banner

--- src/app/home/front-page/banner/banner.component.ts
+++ src/app/home/front-page/banner/banner.component.ts
@@ -9,11 +9,10 @@ declare var $: any;
 export class BannerComponent implements OnInit {
 
   banners:any[] =[
-    {url:'https://s3img.vcdn.vn/123phim/2018/09/mua-viet-tinh-ca-15362925063460.jpg'},
-    {url:'https://s3img.vcdn.vn/123phim/2018/09/meo-map-15362927484218.jpg'},
-    {url:'https://s3img.vcdn.vn/123phim/2018/09/hoan-doi-15362929835425.jpg'},
-    {url:'https://s3img.vcdn.vn/123phim/2018/08/equalizer-2-15357005619311.jpg'},
-    {url:'https://s3img.vcdn.vn/123phim/2018/08/chang-vo-cua-em-15350244757496.jpg'}
+    {url:'https://i.ytimg.com/vi/2VINv0jkmaE/maxresdefault.jpg'},
+    {url:'https://i0.wp.com/vuviphim.com/wp-content/uploads/2018/08/M%C3%A8o-M%E1%BA%ADp-%C4%90i-Ph%C6%B0%E1%BB%A3t-Cats-and-Peachtopia-2018-1.jpg?resize=750%2C463&ssl=1'},
+    {url:'http://image.plo.vn/w653/Uploaded/2018/wopsvun/2018_08_08/hoan-doi_emah.jpg'},
+    {url:'https://cdn.empireonline.com/jpg/70/0/0/1280/960/aspectfit/0/0/0/0/0/0/c/articles/5afc73508a8b8a97068a20b8/equalizer-2-exclusive-quad.jpg'}
   ]
 
   constructor() { }
